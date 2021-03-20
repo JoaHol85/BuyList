72184f37540387324b1c178f181b12cac94bb543
JoaHol85 <71064236+JoaHol85@users.noreply.github.com>
2021-03-20 10:45:48 +0100

    Made it able to put members in Productlists

--- BuyList/Pages/AddMemberToBuyList.cshtml.cs
+++ BuyList/Pages/AddMemberToBuyList.cshtml.cs
@@ -2,15 +2,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BuyList.Data;
+using BuyList.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace BuyList.Pages
 {
     public class AddMemberToBuyListModel : PageModel
     {
-        public void OnGet()
+        private readonly BuyListContext _context;
+        public AddMemberToBuyListModel(BuyListContext context)
         {
+            _context = context;
         }
+        [BindProperty]
+        public string BuyListName { get; set; }
+        [BindProperty]
+        public string MemberId { get; set; }
+
+
+        public List<ProductList> ListOfBuyLists = new List<ProductList>();
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            ListOfBuyLists = await _context.ProductList.ToListAsync();
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            Member member = new Member();
+            var q = await _context.ProductList.FirstOrDefaultAsync(q => q.Name == BuyListName);
+            member.ProductList = q;
+            member.UserId = MemberId;
+            _context.Member.Add(member);
+            await _context.SaveChangesAsync();
+            return Page();
+        }
+
+       
     }
 }
