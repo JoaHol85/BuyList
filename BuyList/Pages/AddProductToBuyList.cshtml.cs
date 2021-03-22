eb8baa593ebd0b356fcc6f6b04005d6ff1bd7f1d
JoaHol85 <71064236+JoaHol85@users.noreply.github.com>
2021-03-22 11:58:43 +0100

    Save before trying to change Models to Many to Many

--- BuyList/Pages/AddProductToBuyList.cshtml.cs
+++ BuyList/Pages/AddProductToBuyList.cshtml.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using BuyList.Data;
 using BuyList.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -13,20 +15,36 @@ namespace BuyList.Pages
     public class AddProductToBuyListModel : PageModel
     {
         private readonly BuyListContext _context;
-        public AddProductToBuyListModel(BuyListContext context)
+        private readonly ApplicationDbContext _appContext;
+        public AddProductToBuyListModel(BuyListContext context, ApplicationDbContext context2)
         {
             _context = context;
+            _appContext = context2;
+
         }
 
         [BindProperty]
         public string BuyListName { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string UserIdString { get; set; }
 
         public List<ProductList> ListOfProductLists { get; set; }
 
         public async Task<IActionResult> OnGetAsync()
         {
             ListOfProductLists = await _context.ProductList.ToListAsync();
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);    //
+            
+           // var q = _context.Member
+
+
             return Page();
+        }
+
+        public void GetUserBuyLists()
+        {
+
+            
         }
     }
 }
